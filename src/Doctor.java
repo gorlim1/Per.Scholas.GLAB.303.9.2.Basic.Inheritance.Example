42d5840ccd7e29deed894443f59ce29f26cecbd6
Desmond Roche <desmondroche@gmail.com>
2024-12-05 04:00:24 -0500

    Added code for this Per Scholas GLAB

---
+++ src/Doctor.java
@@ -0,0 +1,12 @@
+public class Doctor {
+
+    //Declaration, initialization
+    String doctorName;
+    String department;
+
+    //Method to print doctor details
+    public void doctorDetails() {
+
+        System.out.println("Doctor Details...");
+    }
+}
---
+++ src/Surgeon.java
@@ -0,0 +1,8 @@
+public class Surgeon extends Doctor {
+
+    public void surgeonDetails() {
+
+        System.out.println("Surgeon Details...");
+        System.out.println(department = "Cardio");
+    }
+}
